4885c2ac9e7f73407992458f33c33d48df682c50
Felipe Rojas <lordganondorf@gmail.com>
2021-02-10 22:28:55 -0500

    Fix concurrently run

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,26 +1,41 @@
 import { React, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Switch, Route, useHistory, useLocation } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import SearchProducts from "../../components/SearchProducts/SearchProducts";
 import ProductListPage from "../../pages/ProductListPage";
+import ProductPage from "../../pages/ProductPage";
 
 const App = () => {
+  const [productsList, setProductsList] = useState(null)
+  const [selectedProduct, setSelectedProduct] = useState(null)
+  const [searchQuery, setSearchQuery] = useState(null)
+
+  const styles = {
+    display: "block",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <main style={styles}>
+      <header>
         <SearchProducts></SearchProducts>
       </header>
-      <section className="">
+      <section>
         <Router>
           <Switch>
             <Route exact path="/">
+              home
+            </Route>
+            <Route path="/items?search=:query">
               <ProductListPage></ProductListPage>
+            </Route>
+            <Route path="/items/:id">
+              <ProductPage></ProductPage>
             </Route>
           </Switch>
         </Router>
       </section>
-    </div>
+    </main>
   );
 };
 
